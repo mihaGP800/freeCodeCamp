64bcc427444a8ae9077e651be23e56966a613c3f
mihaGP800 <94390495+mihaGP800@users.noreply.github.com>
2022-01-23 22:26:57 +0300

    Add files via upload

---
+++ RecursionRange.js
@@ -0,0 +1,23 @@
+function rangeOfNumbers(startNum, endNum) {
+
+
+  if (startNum === endNum) {
+    return [startNum];
+  } else {
+    
+    const countArray = rangeOfNumbers(startNum, endNum-1);
+    countArray.push(endNum);
+    return countArray;
+  }
+}
+console.log(rangeOfNumbers(3, 6));
+
+
+
+
+
+
+
+
+
+
