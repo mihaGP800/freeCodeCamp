da6e9c708cf09a85b16be95eba956f7d5a52dab2
mihaGP800 <94390495+mihaGP800@users.noreply.github.com>
2022-01-23 15:43:39 +0300

    Add files via upload

---
+++ randomNumbers.js
@@ -0,0 +1,6 @@
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return  console.log(Math.floor(Math.random()*(myMax-myMin+1)) + myMin)
+    // Only change code above this line
+  }
+  randomRange(12,58)
